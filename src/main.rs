9d92aacb598fc7a64dacafd328eb0e535637ffd9
F1L1P <mlodzik.filip@wp.pl>
2023-06-10 16:31:59 +0200

    better i guess

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,44 @@
 extern crate termsize;
+use std::io::{stdout, Write};
 
 fn main() {
-    termsize::get().map(|size| {
+    let size = termsize::get().unwrap();
+
+    loop {
+        let mut stdout = stdout();
+
+        //clear terminal
+        // print!("\x1b[2J");
         for i in 0..size.rows {
             // let mut line = "".to_string();
 
             for j in 0..size.cols {
                 let r = (i as f32 * 255. / (size.rows as f32)) as usize;
                 let g = (j as f32 * 255. / (size.cols as f32)) as usize;
-                print!(
-                "\x1b[48;2;{};{};0m\x1b[38;2;{};{};0m ",
-                r,
-                g,
-                255 - r,
-                255 - g
-                );
+                // print!(
+                // "\x1b[48;2;{};{};0m\x1b[38;2;{};{};0m ",
+                // r,
+                // g,
+                // 255 - r,
+                // 255 - g
+                // );
+
+                stdout
+                    .write(
+                        format!(
+                            "\x1b[48;2;{};{};0m\x1b[38;2;{};{};0m ",
+                            r,
+                            g,
+                            255 - r,
+                            255 - g
+                        )
+                        .as_bytes(),
+                    )
+                    .unwrap();
+
                 // line.push_str(
                 //     format!(
-                //         "\x1b[48;2;{};{};0m\x1b[38;2;{};{};0m ", 
+                //         "\x1b[48;2;{};{};0m\x1b[38;2;{};{};0m ",
                 //         r,
                 //         g,
                 //         255 - r,
@@ -28,7 +49,9 @@ fn main() {
             }
             // line.push_str("\x1b[0m\n");
             // print!("{}", line);
-            print!("\x1b[0m\n");
+            // print!("\x1b[0m\n");
         }
-    });
+
+        stdout.flush().unwrap();
+    }
 }
